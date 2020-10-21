7a54f348644724d06348786a517029208223df48
MarioDexter <757733410@qq.com>
2020-10-22 00:36:55 +0800

    day03

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,12 +4,59 @@
             <div class="avatar_box">
                 <img src="../assets/logo.png" alt="">
             </div>
+            <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+                <el-form-item prop="username">
+                    <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input v-model="loginForm.password" prefix-icon="iconfont icon-password" type="password"></el-input>
+                </el-form-item>
+                <el-form-item class="btns">
+                    <el-button type="primary" @click="login">登录</el-button>
+                    <el-button type="info" @click="resetLoginForm">重置</el-button>
+                </el-form-item>
+            </el-form>
         </div>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    resetLoginForm () {
+      this.$refs.loginFormRef.resetFields()
+      // Object.keys(this.loginForm).forEach(key => this.loginForm[key] === '')
+      for (var key in this.loginForm) {
+        this.loginForm[key] = ''
+      }
+    },
+    login () {
+      this.$refs.loginFormRef.validate(valid => {
+        if (valid) {
+          console.log(valid)
+        }
+      })
+    }
+  }
+}
 </script>
 <style lang="scss" scope>
     .login_content {
@@ -41,6 +88,17 @@ export default {}
                     background-color: #eee;
                 }
             }
+        }
+        .login_form{
+            position: absolute;
+            bottom: 0;
+            box-sizing: border-box;
+            width: 100%;
+            padding: 0 20px;
+        }
+        .btns{
+            display: flex;
+            justify-content: flex-end;
         }
     }
 </style>
